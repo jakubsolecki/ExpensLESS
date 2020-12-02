1be61899bce310524acf5e0cafd87b94eb982859
Krzysztof Nalepa <misiek.nalepa@wp.pl>
2020-12-02 14:24:07 +0100

    addNextAccount function created

--- src/main/java/pl/edu/agh/controller/AccountController.java
+++ src/main/java/pl/edu/agh/controller/AccountController.java
@@ -5,7 +5,9 @@ import javafx.scene.layout.GridPane;
 import lombok.Setter;
 import pl.edu.agh.model.Account;
 import pl.edu.agh.service.AccountService;
-import pl.edu.agh.viewElements.AccountViewElement;
+import pl.edu.agh.viewelements.AccountViewElement;
+
+import java.util.List;
 
 public class AccountController {
     @FXML
@@ -14,15 +16,19 @@ public class AccountController {
     @Setter
     private AccountService accountService;
 
+    private int accountsNumber = 0;
+
+    private void addAccountToPane(Account account){
+        gridPane.add(new AccountViewElement(account), accountsNumber % 3, accountsNumber / 3);
+        accountsNumber++;
+    }
+
+
     @FXML
     public void initialize() {
         new Thread(() -> {
-            int i = 0;
-            for (Account account : accountService.getAllAccounts()){
-                int finalI = i;
-                Platform.runLater(() -> gridPane.add(new AccountViewElement(account), finalI, 0));
-                i++;
-            }
+            List<Account> accountList = accountService.getAllAccounts();
+            Platform.runLater(() -> accountList.forEach(this::addAccountToPane));
         }).start();
     }
 }
