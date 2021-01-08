2e0a19aaeaae5b997b853f8a76a687c937a097ce
Krzysztof Nalepa <misiek.nalepa@wp.pl>
2021-01-08 21:56:09 +0100

    categories working!!! yeeee

--- src/main/java/pl/edu/agh/controller/category/CategoryDialogController.java
+++ src/main/java/pl/edu/agh/controller/category/CategoryDialogController.java
@@ -1,8 +1,10 @@
 package pl.edu.agh.controller.category;
 
+import javafx.collections.FXCollections;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.Node;
+import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 import lombok.Setter;
@@ -17,12 +19,19 @@ import java.math.BigDecimal;
 public class CategoryDialogController {
     @FXML
     public TextField nameTextField;
+    @FXML
+    public ChoiceBox<Type> typeChoiceBox;
 
     @Setter
     private CategoryService categoryService;
 
     @Setter
     private AccountDetailsController accountDetailsController;
+
+    @FXML
+    public void initialize(){
+        typeChoiceBox.setItems(FXCollections.observableArrayList(Type.values()));
+    }
 
     @FXML
     public void handleCancelAction(ActionEvent event) {
@@ -33,7 +42,7 @@ public class CategoryDialogController {
     public void handleAddAction(ActionEvent event) {
         Category category;
         try {
-            category = new Category(nameTextField.getText(), Type.EXPENSE);
+            category = new Category(nameTextField.getText(), typeChoiceBox.getValue());
         } catch (NumberFormatException e){
             System.out.println("Wrong format!");
             return;
